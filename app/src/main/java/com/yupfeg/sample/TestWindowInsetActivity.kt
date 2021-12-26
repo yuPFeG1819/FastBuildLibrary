0efeb94261d26796ce6aea36da5c5b97dd8cf60c
yuPFeG <yupfeg1819@foxmail.com>
2021-12-26 22:57:23 +0800

    测试WindowInset软键盘的支持

--- app/src/main/java/com/yupfeg/sample/TestWindowInsetActivity.kt
+++ app/src/main/java/com/yupfeg/sample/TestWindowInsetActivity.kt
@@ -1,12 +1,18 @@
 package com.yupfeg.sample
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.yupfeg.base.tools.showShortToast
 import com.yupfeg.base.tools.window.*
 import com.yupfeg.base.view.activity.bindingActivity
+import com.yupfeg.base.viewmodel.ext.viewModelDelegate
+import com.yupfeg.base.widget.ext.DSLTextWatcher
+import com.yupfeg.base.widget.ext.hideKeyboard
+import com.yupfeg.base.widget.ext.showKeyboard
 import com.yupfeg.logger.ext.logd
 import com.yupfeg.sample.databinding.ActivityTestWindowInsetBinding
+import com.yupfeg.sample.ui.TestWindowInsetViewModel
 
 /**
  * 测试WindowInsetController
@@ -18,17 +24,18 @@ class TestWindowInsetActivity : AppCompatActivity(){
     private val mBinding : ActivityTestWindowInsetBinding
         by bindingActivity(R.layout.activity_test_window_inset)
 
+    private val mViewModel : TestWindowInsetViewModel by viewModelDelegate()
+
     private var isFullScreen : Boolean = false
     private var isNavigationBar : Boolean = false
+    private var isInput : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding.viewConfig = BindingConfig()
+        mBinding.viewModel = mViewModel
         //允许沉浸式状态栏
         fitImmersiveStatusBar(isDarkText = true)
-        mBinding.viewTestWindowInsetTitleGroup.fitToSystemStatusBar(false)
-        mBinding.viewTestWindowInsetNavigationGroup.fitToSystemNavigationWindow(true)
-
         window.decorView.post {
             logd("当前状态栏高度${this.getStatusBarHeight()} ，" +
                     "当前导航栏高度${this.getNavigationBarHeight()} ，" +
@@ -37,7 +44,23 @@ class TestWindowInsetActivity : AppCompatActivity(){
 
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding.viewConfig = null
+    }
+
     inner class BindingConfig{
+
+        val textWatcher = DSLTextWatcher().apply {
+            afterTextChanged = {
+                logd(it?.toString()?:"")
+            }
+        }
+
+        val onForceChange = View.OnFocusChangeListener { _, hasFocus ->
+            logd("是否获取焦点 $hasFocus")
+            isInput = hasFocus
+        }
 
         fun naviBack(){
             finish()
@@ -45,7 +68,7 @@ class TestWindowInsetActivity : AppCompatActivity(){
 
         fun fullScreen(){
             val pre = isFullScreen
-            logd("change fullScreen mode ${pre}")
+            logd("change fullScreen mode $pre")
             isFullScreen = !pre
             //只能在Android R以上完美实现
             if (isFullScreen){
@@ -72,6 +95,17 @@ class TestWindowInsetActivity : AppCompatActivity(){
 
         fun bottomBtn3(){
             showShortToast("点击了bottomBtn3")
+        }
+
+        fun updateInputState(){
+            if (isInput){
+                mBinding.etTestWindowInsetInput.hideKeyboard()
+                isInput = false
+            }else{
+                mBinding.etTestWindowInsetInput.showKeyboard()
+                isInput = true
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/yupfeg/sample/ui/TestWindowInsetViewModel.kt
@@ -0,0 +1,15 @@
+package com.yupfeg.sample.ui
+
+import androidx.databinding.ObservableField
+import com.yupfeg.base.viewmodel.BaseViewModel
+
+/**
+ *
+ * @author yuPFeG
+ * @date
+ */
+class TestWindowInsetViewModel : BaseViewModel(){
+
+    val onTestInputTest = ObservableField("")
+
+}
