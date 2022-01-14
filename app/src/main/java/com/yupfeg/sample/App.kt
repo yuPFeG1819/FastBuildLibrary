423deacbf0307210c5cc356e20e049086638771e
yuPFeG <yupfeg1819@foxmail.com>
2022-01-14 22:41:36 +0800

    移除无用逻辑

--- app/src/main/java/com/yupfeg/sample/App.kt
+++ app/src/main/java/com/yupfeg/sample/App.kt
@@ -1,10 +1,7 @@
 package com.yupfeg.sample
 
 import com.yupfeg.base.application.BaseApplication
-import com.yupfeg.base.tools.dispatcher.ITask
-import com.yupfeg.base.tools.dispatcher.IdleDelayTaskDispatcher
-import com.yupfeg.base.tools.initToast
-import com.yupfeg.base.tools.pool.WebViewPool
+import com.yupfeg.base.tools.ext.initToast
 import com.yupfeg.logger.ext.setDslLoggerConfig
 import com.yupfeg.logger.printer.LogcatPrinter
 
@@ -12,14 +9,8 @@ class App : BaseApplication(){
 
     override fun onCreate() {
         super.onCreate()
-        initToast()
         initLoggerConfig()
-        val dispatcher = IdleDelayTaskDispatcher().addTask(object : ITask {
-            override fun run() {
-                WebViewPool.prepare(this@App.applicationContext)
-            }
-        })
-        dispatcher.start()
+        initToast()
     }
 
     /**
@@ -30,7 +21,8 @@ class App : BaseApplication(){
             //开启调用位置追踪
             isDisplayClassInfo = true
             //添加线上捕获异常的日志输出
-            logPrinters = listOf(LogcatPrinter(enable = BuildConfig.DEBUG))
+            logPrinters = listOf(LogcatPrinter(enable = true))
         }
     }
+
 }
