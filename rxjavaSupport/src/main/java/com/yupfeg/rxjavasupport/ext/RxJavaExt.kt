87b7b90df5396065edf63f87afd5f9c6771f12ef
yuPFeG <yupfeg1819@foxmail.com>
2022-01-14 22:28:13 +0800

    移除并替换本地线程池

--- rxjavaSupport/src/main/java/com/yupfeg/rxjavasupport/ext/RxJavaExt.kt
+++ rxjavaSupport/src/main/java/com/yupfeg/rxjavasupport/ext/RxJavaExt.kt
@@ -1,8 +1,11 @@
 package com.yupfeg.rxjavasupport.ext
 
-import com.yupfeg.base.tools.pool.GlobalLocalThreadPool
+import com.yupfeg.dispatcher.ExecutorProvider
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Maybe
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 import java.util.concurrent.Executor
@@ -46,34 +49,34 @@ inline fun <reified T> BehaviorSubject<T>.copyMapFromList(map: (T) -> List<T>) {
 /**
  * [Completable]拓展方法，将上游事件源置于子线程
  * * 在一个事件流中仅能生效一次
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun Completable.upToBackThread(
-    targetExecutor : Executor = GlobalLocalThreadPool.executorService
+    targetExecutor : Executor = ExecutorProvider.ioExecutor
 ) : Completable {
     return this.subscribeOn(Schedulers.from(targetExecutor))
 }
 
 /**
  * [Completable]拓展函数，将下游事件源置于子线程
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  *
  * */
 @Suppress("unused")
 fun Completable.downToBackThread(
-    targetExecutor : Executor = GlobalLocalThreadPool.executorService
+    targetExecutor : Executor = ExecutorProvider.ioExecutor
 ) : Completable {
     return this.observeOn(Schedulers.from(targetExecutor))
 }
 
 /**
  * [Flowable]类型的拓展函数，将上游事件源置于子线程，下游置于主线程
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  */
 @Suppress("unused")
 fun Completable.upToBackThreadDownToMain(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Completable {
     return this.compose{
         it.subscribeOn(Schedulers.from(targetExecutor))
@@ -95,22 +98,22 @@ fun Completable.downToMainThread() : Completable{
 /**
  * [Maybe]拓展方法，将上游事件源置于子线程
  * * 在一个事件流中仅能生效一次
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun <T : Any> Maybe<T>.upToBackThread(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Maybe<T> {
     return this.subscribeOn(Schedulers.from(targetExecutor))
 }
 
 /**
  * [Maybe]的拓展函数，将下游事件源置于子线程
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun <T : Any> Maybe<T>.downToBackThread(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Maybe<T>{
     return this.observeOn(Schedulers.from(targetExecutor))
 }
@@ -125,11 +128,11 @@ fun <T : Any> Maybe<T>.downToMainThread() : Maybe<T>{
 
 /**
  * [Maybe]的拓展函数，将上游事件源置于子线程，下游置于主线程
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  */
 @Suppress("unused")
 fun <T : Any> Maybe<T>.upToBackThreadDownToMain(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Maybe<T> {
     return this.compose {
         it.subscribeOn(Schedulers.from(targetExecutor))
@@ -144,22 +147,22 @@ fun <T : Any> Maybe<T>.upToBackThreadDownToMain(
 /**
  * [Observable]拓展函数，将上游事件源置于子线程
  * * 在一个事件流中仅能生效一次
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun <T : Any> Observable<T>.upToBackThread(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Observable<T> {
     return this.subscribeOn(Schedulers.from(targetExecutor))
 }
 
 /**
  * [Observable]拓展函数，将下游事件源置于子线程
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun <T : Any> Observable<T>.downToBackThread(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Observable<T>{
     return this.observeOn(Schedulers.from(targetExecutor))
 }
@@ -174,11 +177,11 @@ fun <T : Any> Observable<T>.downToMainThread() : Observable<T>{
 
 /**
  * [Observable]拓展函数，将上游事件源置于子线程，下游置于主线程
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  */
 @Suppress("unused")
 fun <T : Any> Observable<T>.upToBackThreadDownToMain(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Observable<T> {
     return this.compose {
         it.subscribeOn(Schedulers.from(targetExecutor))
@@ -194,22 +197,22 @@ fun <T : Any> Observable<T>.upToBackThreadDownToMain(
 /**
  * [Flowable]拓展函数，将上游事件源置于子线程
  * * 在一个事件流中仅能生效一次
- * @param targetExecutor 目标子线程的线程池,默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池,默认使用[ExecutorProvider.ioExecutor]
  * */
 @Suppress("unused")
 fun <T : Any> Flowable<T>.upToBackThread(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Flowable<T> {
     return this.subscribeOn(Schedulers.from(targetExecutor))
 }
 
 /**
  * [Flowable]类型的拓展函数，将上游事件源置于子线程，下游置于主线程
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  */
 @Suppress("unused","SpellCheckingInspection")
 fun <T : Any> Flowable<T>.upToBackThreadDownToMain(
-    targetExecutor: Executor = GlobalLocalThreadPool.executorService
+    targetExecutor: Executor = ExecutorProvider.ioExecutor
 ) : Flowable<T> {
     return this.compose{
         it.subscribeOn(Schedulers.from(targetExecutor))
@@ -219,11 +222,11 @@ fun <T : Any> Flowable<T>.upToBackThreadDownToMain(
 
 /**
  * [Flowable]拓展函数，将下游事件置于子线程
- * @param targetExecutor 目标子线程的线程池，默认使用[GlobalLocalThreadPool.executorService]
+ * @param targetExecutor 目标子线程的线程池，默认使用[ExecutorProvider.ioExecutor]
  */
 @Suppress("unused","SpellCheckingInspection")
 fun <T : Any> Flowable<T>.downToBackThread(
-    targetExecutor : Executor = GlobalLocalThreadPool.executorService
+    targetExecutor : Executor = ExecutorProvider.ioExecutor
 ) : Flowable<T> {
     return observeOn(Schedulers.from(targetExecutor))
 }
