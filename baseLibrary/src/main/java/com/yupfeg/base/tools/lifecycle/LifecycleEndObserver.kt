4e205a7a1043729dbf1a10dd2485adf62aaca71d
yuPFeG <yupfeg1819@foxmail.com>
2022-03-06 09:31:13 +0800

    修改变量命名

--- baseLibrary/src/main/java/com/yupfeg/base/tools/lifecycle/LifecycleEndObserver.kt
+++ baseLibrary/src/main/java/com/yupfeg/base/tools/lifecycle/LifecycleEndObserver.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.LifecycleOwner
  */
 class LifecycleEndObserver(
     private val endState : Lifecycle.State = Lifecycle.State.DESTROYED,
-    private val onEndAction : ()->Unit
+    private val doOnEndAction : ()->Unit
 ) : LifecycleEventObserver {
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
@@ -19,12 +19,12 @@ class LifecycleEndObserver(
         if (currentState == Lifecycle.State.DESTROYED) {
             //视图已销毁
             source.lifecycle.removeObserver(this)
-            onEndAction.invoke()
+            doOnEndAction.invoke()
             return
         }
         //在指定生命周期会销毁引用，防止内存泄漏
         if (currentState == endState) {
-            onEndAction.invoke()
+            doOnEndAction.invoke()
         }
     }
 
