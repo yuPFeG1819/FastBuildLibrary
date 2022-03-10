97e3482236ebaef57c3cfd640e1b60ee3c7eaf69
yuPFeG <yupfeg1819@foxmail.com>
2022-03-10 17:25:47 +0800

    优化重命名自动移除的生命周期状态的订阅监听

--- baseLibrary/src/main/java/com/yupfeg/base/tools/lifecycle/AutoLifecycleStateObserver.kt
+++ baseLibrary/src/main/java/com/yupfeg/base/tools/lifecycle/AutoLifecycleStateObserver.kt
@@ -5,13 +5,13 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 
 /**
- * 对指定生命周期视图生命周期结束的监听
+ * 对指定视图生命周期状态的监听
  * @author yuPFeG
  * @date 2021/03/15
  */
-class LifecycleEndObserver(
-    private val endState : Lifecycle.State = Lifecycle.State.DESTROYED,
-    private val doOnEndAction : ()->Unit
+class AutoLifecycleStateObserver(
+    private val targetState : Lifecycle.State = Lifecycle.State.DESTROYED,
+    private val doOnStateAction : ()->Unit
 ) : LifecycleEventObserver {
 
     override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
@@ -19,12 +19,14 @@ class LifecycleEndObserver(
         if (currentState == Lifecycle.State.DESTROYED) {
             //视图已销毁
             source.lifecycle.removeObserver(this)
-            doOnEndAction.invoke()
+            if (targetState == Lifecycle.State.DESTROYED){
+                doOnStateAction.invoke()
+            }
             return
         }
-        //在指定生命周期会销毁引用，防止内存泄漏
-        if (currentState == endState) {
-            doOnEndAction.invoke()
+        //在指定生命周期执行操作
+        if (currentState == targetState) {
+            doOnStateAction.invoke()
         }
     }
 
