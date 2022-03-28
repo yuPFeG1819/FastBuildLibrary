1bfc8787c9d3ad7dd8d750f4ac3855b27ad06310
yuPFeG <yupfeg1819@foxmail.com>
2022-03-28 22:55:04 +0800

    1. 移除demo里的BaseApplication依赖 2. 测试应用前后台监控功能

--- app/src/main/java/com/yupfeg/sample/App.kt
+++ app/src/main/java/com/yupfeg/sample/App.kt
@@ -1,19 +1,41 @@
 package com.yupfeg.sample
 
-import com.yupfeg.base.application.BaseApplication
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.multidex.MultiDex
 import com.yupfeg.base.tools.ext.initToast
+import com.yupfeg.base.tools.process.ProcessLifecycleOwner
+import com.yupfeg.logger.ext.logd
 import com.yupfeg.logger.ext.setDslLoggerConfig
 import com.yupfeg.logger.printer.LogcatPrinter
 import com.yupfeg.remote.tools.handler.GlobalHttpResponseProcessor
 import com.yupfeg.sample.tools.GlobalResponseHandler
+import kotlin.properties.Delegates
 
-class App : BaseApplication(){
+class App : Application(){
+
+    companion object{
+        var INSTANCE : Application by Delegates.notNull()
+            private set
+
+        var appContext : Context by Delegates.notNull()
+            private set
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 
     override fun onCreate() {
         super.onCreate()
+        INSTANCE = this
+        appContext = this.applicationContext
         initLoggerConfig()
         initToast()
-
+        initProcessLifecycleOwner()
         //设置全局http响应
         GlobalHttpResponseProcessor.setResponseHandler(GlobalResponseHandler())
     }
@@ -28,6 +50,19 @@ class App : BaseApplication(){
             //添加线上捕获异常的日志输出
             logPrinters = listOf(LogcatPrinter(enable = true))
         }
+    }
+
+    private fun initProcessLifecycleOwner(){
+        ProcessLifecycleOwner.init(this)
+        ProcessLifecycleOwner.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onStart(owner: LifecycleOwner) {
+                logd("进入前台")
+            }
+
+            override fun onStop(owner: LifecycleOwner) {
+                logd("进入后台")
+            }
+        })
     }
 
 }
